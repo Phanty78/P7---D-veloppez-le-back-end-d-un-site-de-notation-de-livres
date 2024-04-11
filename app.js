d8317b9dbad6f786cee125f9b55bb3a10825fad6
Mael Donnart <maeldonnart@hotmail.fr>
2024-04-11 11:28:37 +0200

    - code des Routes de book.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose')
 const app = express();
 
+const bookRoutes = require('./routes/book')
 const userRoutes= require('./routes/user')
 
 const mongoDB = 'mongodb+srv://testuser:mvpZ3PjR9KGEEAsc@cluster0.jjdtnp5.mongodb.net/';
@@ -20,6 +21,7 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use('/api/books', bookRoutes)
 app.use('/api/auth', userRoutes)
 
 module.exports = app;
---
+++ routes/book.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const router = express.Router()
+const bookCtrl = require('../controllers/book')
+
+router.get('/', bookCtrl.getBooks)
+router.get('/:id', bookCtrl.getBook)
+router.get('/bestrating', bookCtrl.getBestRating)
+router.post('/', auth, multer, bookCtrl.saveNewBook)
+router.put('/:id', auth, multer, bookCtrl.updateBook)
+router.delete('/:id', auth, bookCtrl.deleteBook)
+router.post('/:id/rating', auth, bookCtrl.ratingbook)
+
+module.exports = router
