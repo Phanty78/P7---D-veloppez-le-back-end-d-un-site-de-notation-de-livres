b53280b035787b20602b627f83f6edb8b51a7438
Mael Donnart <maeldonnart@hotmail.fr>
2024-04-11 12:39:52 +0200

    - ajout du code du controllers pour getBooks - ajout de auth.js

--- controllers/book.js
+++ controllers/book.js
@@ -1,1 +1,19 @@
 const Book = require('../models/Book')
+
+exports.getBooks = (req, res, next) => {
+    Book.find().then(
+      (books) => {
+        res.status(200).json(books);
+      }
+    ).catch(
+      (error) => {
+        res.status(400).json({
+          error: error
+        });
+      }
+    );
+  }
+
+  exports.getBook = (req, res, next) => {
+    Book.findOne
+  }
--- routes/book.js
+++ routes/book.js
@@ -1,13 +1,19 @@
 const express = require('express')
 const router = express.Router()
+
 const bookCtrl = require('../controllers/book')
+
+const auth = require('../middleware/auth')
+
 
 router.get('/', bookCtrl.getBooks)
 router.get('/:id', bookCtrl.getBook)
+/*
 router.get('/bestrating', bookCtrl.getBestRating)
 router.post('/', auth, multer, bookCtrl.saveNewBook)
 router.put('/:id', auth, multer, bookCtrl.updateBook)
 router.delete('/:id', auth, bookCtrl.deleteBook)
 router.post('/:id/rating', auth, bookCtrl.ratingbook)
+*/
 
 module.exports = router
