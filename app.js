74d3faba60df0e3d7c23a7966c13e4d4e54e2373
Mael Donnart <maeldonnart@hotmail.fr>
2024-04-11 11:06:47 +0200

    - création de la structure du projet - création des fichiers coté backend - mise en place des routes user - mise ne place des models user et book - mise en place de server.js - mise en place de la politique de CORS

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose')
+const app = express();
+
+const userRoutes= require('./routes/user')
+
+const mongoDB = 'mongodb+srv://testuser:mvpZ3PjR9KGEEAsc@cluster0.jjdtnp5.mongodb.net/';
+mongoose.connect(mongoDB);
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', function() {
+  console.log("Connected successfully to MongoDB");
+});
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+app.use('/api/auth', userRoutes)
+
+module.exports = app;
